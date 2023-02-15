8c0526fa49e7716980b864f24330455775e11f71
Berkcanist <berkcan.0942@gmail.com>
2023-02-16 00:33:08 +0300

    python1

---
+++ for.py
@@ -0,0 +1,20 @@
+# numbers = [1,2,3,4,5]
+# for num in numbers:
+#     print(num)
+
+# names = ['Berk', 'Can', 'Demir']
+# for name in names:
+#     print(f'My name is {name}')
+
+# name = 'Berkcan Demir'
+# for word in name:
+#     print(word)
+
+# tuple = [(1,2),(3,4),(5,6)]
+# for t,n in tuple:
+#     print(t,n)
+
+d = {'k1':1, 'k2':2, 'k3':3}
+#for n in d:
+for key,value in d.items():
+    print(key,'=' ,value)
---
+++ for_demo.py
@@ -0,0 +1,50 @@
+sayilar = [1,3,5,7,9,12,19,21]
+
+#1
+# for uc in sayilar:
+#     if uc % 3 == 0:
+#         print(f"{uc} -> 3'ün katıdır")
+#     else:
+#         print(f"{uc} -> 3'ün katı değildir")
+
+
+#2
+# toplam = 0
+# for sayi in sayilar:
+#     toplam += sayi
+# print(toplam)
+
+#3
+# for sayi in sayilar:
+#     if sayi % 2 == 1:
+#         sayi = sayi**2
+#         print(sayi)
+
+sehirler = ['kocaeli', 'istanbul', 'ankara', 'izmir', 'rize', 'ağrı', 'kesici']
+
+# for x in sehirler:
+#     i = 0
+#     for y in x:
+#         i = i + 1
+#     if i <= 6:
+#         print(x)
+
+urunler = [
+    {'name':'samsung S5', 'price': '3000'},
+    {'name':'samsung S6', 'price': '4000'},
+    {'name':'samsung S7', 'price': '5000'},
+    {'name':'samsung S8', 'price': '6000'},
+    {'name':'samsung S9', 'price': '7000'}
+]
+
+#5
+# toplam = 0
+# for telefon in urunler:
+#     #print(telefon['price'])
+#     toplam += int(telefon['price'])
+# print(toplam)
+
+#6
+# for telefon in urunler:
+#     if int(telefon['price']) <= 5000:
+#         print(telefon['name'])
